b5313095d65d263631534327f38c804b7e9698cd
kaushik <kaushikala.ala30@gmail.com>
2019-01-17 18:32:36 -0600

    New project

---
+++ src/com/sq/learn/ArthmeticOperations.java
@@ -0,0 +1,12 @@
+package com.sq.learn;
+
+public class ArthmeticOperations {
+
+	public static void main (String args[]) {
+		
+		System.out.println("Hello World ! Welcome to Java");
+		
+	}
+	
+	
+}
